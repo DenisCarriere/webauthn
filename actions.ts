1e82483b3c4ee459e2540ce331e358ff3506387d
Denis Carriere <carriere.denis@gmail.com>
2021-10-02 10:54:57 -0400

    add actions

--- actions.ts
+++ actions.ts
@@ -1,4 +1,4 @@
-import { Struct, Name, Asset, Action } from '@greymass/eosio';
+import { Struct, Name, Asset, Action, NameType, AssetType } from '@greymass/eosio';
 
 @Struct.type('transfer')
 class Transfer extends Struct {
@@ -8,15 +8,10 @@ class Transfer extends Struct {
   @Struct.field('string') memo!: string;
 }
 
-export function transfer(from: Name, to: Name, quantity: Asset, memo: string) {
+export function transferAction( account: NameType, from: NameType, to: NameType, quantity: AssetType, memo: string ): Action {
   return Action.from({
-    authorization: [
-      {
-        actor: from,
-        permission: 'active',
-      },
-    ],
-    account: 'eosio.token',
+    authorization: [ { actor: from, permission: "active" } ],
+    account,
     name: 'transfer',
     data: Transfer.from({
       from,
@@ -24,5 +19,8 @@ export function transfer(from: Name, to: Name, quantity: Asset, memo: string) {
       quantity,
       memo,
     }),
-  });
+  })
 }
+
+const action = transferAction("eosio.token", "myaccount", "toaccount", "1.0000 EOS", "foo");
+console.log(action.toJSON())
--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 import { APIClient } from '@greymass/eosio';
+import fetch from "isomorphic-fetch";
 
-export const client = new APIClient({ url: 'https://jungle3.greymass.com' });
+export const client = new APIClient({ url: 'https://jungle3.greymass.com', fetch });
